9c5813cf891f2f850851adb3fd3db83101906822
Wafa P M <pmwafa@gmail.com>
2021-09-29 23:15:56 +0530

    review and rating

--- accounts/models.py
+++ accounts/models.py
@@ -58,6 +58,9 @@ class AccountUser(AbstractBaseUser):
 
     objects = AccountUserManager()
 
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
     def __str__(self):
         return self.email
 
