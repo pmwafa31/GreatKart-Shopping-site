ea32c41a45d1447bd7d05cd95944dff0139b791b
Wafa P M <pmwafa@gmail.com>
2022-02-07 20:30:21 +0530

    corrections made

--- Greatkart_Project/settings.py
+++ Greatkart_Project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [' greatkart-env.eba-aguwkbjp.us-west-2.elasticbeanstalk.com',"*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -89,19 +89,7 @@ AUTH_USER_MODEL = 'accounts.AccountUser'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
